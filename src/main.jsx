5c45017eec097e46b626395b0159a6b91600698c
Nada Wael <nadawael3539@gmail.com>
2024-10-03 00:52:10 +0300

    install bootsrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { appRouter } from './router' 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
 createRoot(document.getElementById('root')).render(
