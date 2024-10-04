79ab682f96c2904376725ffd2da595ca3dff10d0
Khaled Hany <kh6379338@gmail.com>
2024-10-04 04:25:42 +0300

    finish the home page by khaled

---
+++ src/pages/home-page/constants.js
@@ -0,0 +1,66 @@
+export const portfolioItems = [
+{
+    mainImage: "src/images/profileONE.png",
+    starsImage: "src/images/stars/starsONE.png",
+    title: " “I love it! No more air fresheners”",
+    description: "Luisa"
+},
+{
+    mainImage: "src/images/profileTWO.png",
+    starsImage: "src/images/stars/starsTWO.png",
+    title: " “Racomended for everyone”",
+    description: "Edoardo"
+},
+{
+    mainImage: "src/images/profileTHREE.png",
+    starsImage: "src/images/stars/starsTHREE.png",
+    title: " “Looks very natural, the smell is awesome”",
+    description: "Mart"
+}
+]
+
+export const productItems = [
+    {
+       imageLink: "src/images/candelCups/cup1.png",
+       productName:"Spiced Mint",
+       priceNum:"9.99",
+    },
+    {
+        imageLink: "src/images/candelCups/cup2.png",
+        productName:"Spiced Mint",
+        priceNum:"11",
+     },
+     {
+        imageLink: "src/images/candelCups/cup3.png",
+        productName:"Spiced Mint",
+        priceNum:"9.99",
+     },
+     {
+         imageLink: "src/images/candelCups/cup4.png",
+         productName:"Spiced Mint",
+         priceNum:"9.99",
+      },
+]
+
+export const productItems2 = [
+    {
+       imageLink: "src/images/candelCups/cup5.png",
+       productName:"Spiced Mint",
+       priceNum:"9.99",
+    },
+    {
+        imageLink: "src/images/candelCups/cup6.png",
+        productName:"Spiced Mint",
+        priceNum:"10",
+     },
+     {
+        imageLink: "src/images/candelCups/cup7.png",
+        productName:"Spiced Mint",
+        priceNum:"9.99",
+     },
+     {
+         imageLink: "src/images/candelCups/cup8.png",
+         productName:"Spiced Mint",
+         priceNum:"9.99",
+      },
+]
--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import { portfolioItems, productItems, productItems2 } from "./constants";
 
 const HomePage = () => {
   return (
     <>
-      {/* <h1>Welcome to the Home Page</h1> */}
-
-      <div className="imageContainer">
-        {/* <img className="imageONE" src="src/images/image1.jpeg" alt="not found" /> */}
-        <div className="section-one rounded border">
+      {/* section-ONE DIV */}
+      <div className="imageContainer d-flex align-items-center justify-content-center flex-column">
+        <div className="section-one rounded border w-35 text-center">
           <h1>
             🌱 <br />
             The nature candle
@@ -17,7 +16,11 @@ const HomePage = () => {
             All handmade with natural soy wax, Candleaf is a companion for all
             your pleasure moments{" "}
           </p>
-          <button type="button" class="btn btn-success" data-bs-toggle="button">
+          <button
+            type="button"
+            className="btn btn-success"
+            data-bs-toggle="button"
+          >
             <Link to="/products" className="text-white text-decoration-none ">
               Discovery our collection
             </Link>
@@ -25,127 +28,174 @@ const HomePage = () => {
         </div>
       </div>
 
-
-
-    {/*sectionTWO DIV */}
-    <div>
-
-        <div className="secTwo-div1">
+      {/*section-TWO DIV */}
+      <div>
+        <div className="gap-2 d-flex text-uppercase align-items-center justify-content-center flex-column p-5">
           <h1>Products</h1>
           <p>Order it for you or for your beloved ones </p>
           <hr />
         </div>
         {/*the first 4 cards */}
-        <div className="cards">
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
+        <div className="gap-5 d-flex justify-content-center">
+          {productItems.map(({ imageLink, productName, priceNum }, index) => {
+            return (
+              <Link
+                className="text-decoration-none text-black item-card d-flex justify-content-center flex-column align-items-center border rounded"
+                key={index}
+              >
+                <img
+                  width={100}
+                  height={150}
+                  src={imageLink}
+                  alt="not found"
+                  className="w-100 rounded-top"
+                />
+                <div className="p-2 bg-success w-100 rounded-bottom">
+                  <h5 className="text-start">{productName}</h5>
+                  <p className="text-end">{priceNum}$</p>
+                </div>
+              </Link>
+            );
+          })}
         </div>
         {/*the second 4 cards */}
-        <div className="cards">
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
-          <Link className="text-decoration-none text-black item-card">
-            <img
-              // className="small_IMAGES"
-              width={100}
-              height={100}
-              src="src/images/image1.jpeg"
-              alt="not found"
-            />
-            <div className="p-2 bg-success">
-              <h5 className="text-start">Spiced Mint</h5>
-              <p class="text-end">9.99$</p>
-            </div>
-          </Link>
+        <div className="gap-5 py-5 d-flex justify-content-center">
+          {productItems2.map(({ imageLink, productName, priceNum }, index) => {
+            return (
+              <Link
+                className="text-decoration-none text-black item-card d-flex justify-content-center flex-column align-items-center "
+                key={index}
+              >
+                <img
+                  width={100}
+                  height={150}
+                  src={imageLink}
+                  alt="not found"
+                  className="w-100 rounded-top"
+                />
+                <div className="p-2 bg-success w-100 border rounded-bottom">
+                  <h5 className="text-start">{productName}</h5>
+                  <p className="text-end">{priceNum}$</p>
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </div>
+
+      {/*section-three DIV */}
+      <div className="d-flex gap-5">
+        <div className="w-50">
+          <img className="w-100" src="src/images/candle.png" alt="not found" />
+        </div>
+
+        <div className="d-flex flex-column justify-content-center">
+          <h1>
+            Clean and <br />
+            fragrant soy wax
+          </h1>
+          <p className="text-success fw-medium">
+            {" "}
+            Made for your home and for your wellness
+          </p>
+          <br />
+          <ul>
+            <li>
+              <p>
+                <span className="fw-bold">Eco-sustainable:</span>All recyclable
+                materials, 0% CO2 emissions
+              </p>
+            </li>
+
+            <li>
+              <p>
+                <span className="fw-bold">Hypoallergenic:</span>100% natural,
+                human friendly ingredients
+              </p>
+            </li>
+
+            <li>
+              {" "}
+              <p>
+                <span className="fw-bold">Handmade:</span> All candles are
+                craftly made with love.
+              </p>
+            </li>
+
+            <li>
+              <p>
+                <span className="fw-bold">Long burning:</span>No more waste.
+                Created for last long.
+              </p>
+            </li>
+          </ul>
+          <button className="btn btn-success">Learn more</button>
+        </div>
+      </div>
+
+      {/*section-FOUR DIV */}
+      <div className="secFOUR-div1 p-5">
+        <div>
+          <h1 className="d-flex justify-content-center">Testimonials</h1>
+
+          <p className="d-flex justify-content-center">
+            Some quotes from our happy customers
+          </p>
+        </div>
+
+        <div className="d-flex gap-5 justify-content-center py-4">
+          {portfolioItems.map(
+            ({ mainImage, starsImage, title, description }, index) => (
+              <div
+                className="w-25 d-flex flex-column justify-content-center align-items-center border border-success bg-white rounded px-5"
+                key={index}
+              >
+                <img className="w-100 " src={mainImage} alt="not defied " />
+                <img className="w-100" src={starsImage} alt="" />
+                <p className="m-3 text-center fw-bold">
+                  {title}
+                  <span>
+                    <h3 className="fw-light fs-6">{description}</h3>
+                  </span>
+                </p>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+
+      <div>
+        <div className="gap-2 d-flex text-uppercase align-items-center justify-content-center flex-column pt-5">
+          <h1>Popular</h1>
+          <p>Our top selling product that you may like</p>
+          <hr />
+        </div>
+        {/*the first 4 cards */}
+        <div className="gap-5 d-flex justify-content-center pb-5">
+          {productItems.map(({ imageLink, productName, priceNum }, index) => {
+            return (
+              <Link
+                className="text-decoration-none text-black item-card d-flex justify-content-center flex-column align-items-center border rounded"
+                key={index}
+              >
+                <img
+                  width={100}
+                  height={150}
+                  src={imageLink}
+                  alt="not found"
+                  className="w-100 rounded-top"
+                />
+                <div className="p-2 bg-success w-100 rounded-bottom">
+                  <h5 className="text-start">{productName}</h5>
+                  <p className="text-end">{priceNum}$</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      <div></div>
     </>
   );
 };
